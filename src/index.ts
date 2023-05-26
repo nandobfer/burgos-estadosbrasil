77a64982637260f85abc756f9ce79526c391b59a
nandobfer <nandobfer@yahoo.com.br>
2023-05-26 00:24:57 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+﻿export { useEstadosBrasil } from './useEstadosBrasil'
---
+++ src/useEstadosBrasil.ts
@@ -0,0 +1,39 @@
+export interface EstadosBrasil {
+    id: number
+    value: string
+    label: string
+}
+
+export const useEstadosBrasil = () => {
+    const estados = [
+        {id: 1, value:"AC", label: "Acre"},
+        {id: 2, value:"AL", label: "Alagoas"},
+        {id: 3, value:"AP", label: "Amapá"},
+        {id: 4, value:"AM", label: "Amazonas"},
+        {id: 5, value:"BA", label: "Bahia"},
+        {id: 5, value:"CE", label: "Ceará"},
+        {id: 6, value:"DF", label: "Distrito Federal"},
+        {id: 7, value:"ES", label: "Espírito Santo"},
+        {id: 8, value:"GO", label: "Goiás"},
+        {id: 9, value:"MA", label: "Maranhão"},
+        {id: 10, value:"MT", label: "Mato Grosso"},
+        {id: 11, value:"MS", label: "Mato Grosso do Sul"},
+        {id: 12, value:"MG", label: "Minas Gerais"},
+        {id: 13, value:"PA", label: "Pará"},
+        {id: 14, value:"PB", label: "Paraíba"},
+        {id: 15, value:"PR", label: "Paraná"},
+        {id: 16, value:"PE", label: "Pernambuco"},
+        {id: 17, value:"PI", label: "Piauí"},
+        {id: 18, value:"RJ", label: "Rio de Janeiro"},
+        {id: 19, value:"RN", label: "Rio Grande do Norte"},
+        {id: 20, value:"RS", label: "Rio Grande do Sul"},
+        {id: 21, value:"RO", label: "Rondônia"},
+        {id: 22, value:"RR", label: "Roraima"},
+        {id: 23, value:"SC", label: "Santa Catarina"},
+        {id: 24, value:"SP", label: "São Paulo"},
+        {id: 25, value:"SE", label: "Sergipe"},
+        {id: 26, value:"TO", label: "Tocantins"},
+    ]
+    
+    return estados
+}
